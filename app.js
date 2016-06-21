a762002cae3f99ade1b84e1c5dbca87c10eca3d5
poorani11 <poorani.mgr@gmail.com>
2016-06-21 09:23:24 +0530

    finally able to get data from the api

---
+++ app.js
@@ -0,0 +1,38 @@
+// MODULE
+var rssApp = angular.module('rssApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+rssApp.config(function ($routeProvider){
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    }) 
+});                  
+// CONTROLLERS
+rssApp.controller('homeController', ['$scope','$http', function($scope,$http){
+  $scope.getItem = function(){
+    var url = 'http://rss.cnn.com/rss/edition.rss'
+  feednami.load(url,function(result){
+    if(result.error){
+      console.log(result.error)
+    }
+    else{
+      $scope.entries = result.feed.entries;
+      console.log($scope.entries);
+      for(var i = 0; i < $scope.entries.length; i++){
+        var entry = $scope.entries[i]
+        console.log(entry.title);
+      }
+    }
+  })
+};
+$scope.getItem();
+
+}]);
+
+
+
+
