07f341f346ebab97c82a4dbd0a31850398da0b05
poorani11 <poorani.mgr@gmail.com>
2016-06-21 12:14:39 +0530

    Added localstorage option to access data on low internet connection

--- app.js
+++ app.js
@@ -18,17 +18,21 @@ rssApp.controller('homeController', ['$scope','$http', function($scope,$http){
   feednami.load(url,function(result){
     if(result.error){
       console.log(result.error)
+      if(window.localStorage["entries"] !== undefined){
+        $scope.entries = JSON.parse(window.localStorage["entries"]);
+      }
     }
     else{
       $scope.entries = result.feed.entries;
+      window.localStorage["entries"] = JSON.stringify(result.feed.entries);
        $scope.$apply(function () {
            console.log($scope.entries);
         });
-      for(var i = 0; i < $scope.entries.length; i++){
-        var entry = $scope.entries[i]
-        console.log(entry.title);
-         console.log(entry.summary) 
-      }
+      // // for(var i = 0; i < $scope.entries.length; i++){
+      // //   var entry = $scope.entries[i]
+      // //   console.log(entry.title);
+      // //    console.log(entry.summary) 
+      // }
     }
   })
 };
